9540d29d1a680db52a3a5766ae8b3180791e3a85
Merrick Brown <merrick.br@gmail.com>
2021-09-28 13:39:30 -0400

    externalize some resources

--- vite.config.js
+++ vite.config.js
@@ -6,24 +6,25 @@ import reactRefresh from "@vitejs/plugin-react-refresh";
 export default defineConfig({
   assetsInclude: ['./src/data/**.json'],
   plugins: [reactRefresh()],
-//   rollupOptions: {
-//     external: [
-//       //'react', 
-//       //'react-dom',
-      
-//     ],
-//     globals: {
-//       //'react': 'React',
-//       //'react-dom': 'ReactDOM'
-//     },
-//   },
+  rollupOptions: {
+    external: [
+      'react', 
+      'react-dom',
+      'plotly.js-basic-dist-min',
+    ],
+    globals: {
+      'react': 'React',
+      'react-dom': 'ReactDOM',
+      'plotly.js-basic-dist-min': 'Plotly'
+    },
+  },
   build: {
     outDir: "build"
   },
   server: {
-    strictPort: true,
-    hmr: {
-      port: 443 // Run the websocket server on the SSL port
-    }
+    strictPort: true
+    //hmr: {
+    //  port: 443 // Run the websocket server on the SSL port
+    //}
   }
 });
